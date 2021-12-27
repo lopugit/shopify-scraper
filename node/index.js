2fd3d03370e92cbedfeb2defc4bc2423fa27e2b2
Nikolaj Frey <nikolaj@thirstcreative.com.au>
2021-12-27 23:52:28 +1100

    main

--- node/index.js
+++ node/index.js
@@ -28,53 +28,55 @@ app.get('/v1/videos', async (req, res) => {
 		let videos = []
 
 		for (let playlistId of playlistIds) {
-			console.log('Trying to get channel id from username', playlistId)
-			let usernameResp = await axios.get('https://www.googleapis.com/youtube/v3/channels', {
-				params: {
-					part: 'id',
-					key: process.env.API_KEY,
-					forUsername: playlistId
-				}
-			}).catch(err => {
-				console.error(err)
-			})
-			if (usernameResp && usernameResp.data && usernameResp.data.pageInfo.totalResults > 0) {
-				playlistId = usernameResp.data.items[0].id
-			} else {
-				let searchResp = await axios.get('https://www.googleapis.com/youtube/v3/search', {
+			if (playlistId.length) {
+				console.log('Trying to get channel id from username', playlistId)
+				let usernameResp = await axios.get('https://www.googleapis.com/youtube/v3/channels', {
 					params: {
-						part: 'id,snippet',
-						maxResults: 1,
-						q: playlistId,
+						part: 'id',
+						key: process.env.API_KEY,
+						forUsername: playlistId
+					}
+				}).catch(err => {
+					console.error(err)
+				})
+				if (usernameResp && usernameResp.data && usernameResp.data.pageInfo.totalResults > 0) {
+					playlistId = usernameResp.data.items[0].id
+				} else if (!(playlistId[0] === 'U' && playlistId[1] === 'U')){
+					console.log('Didn\'t find a channel for username', playlistId, 'trying generalised search')
+					let searchResp = await axios.get('https://www.googleapis.com/youtube/v3/search', {
+						params: {
+							part: 'id,snippet',
+							maxResults: 1,
+							q: playlistId,
+							key: process.env.API_KEY
+						}
+					})
+					if (searchResp && searchResp.data?.items?.length) {
+						playlistId = searchResp.data.items[0].snippet.channelId
+					}
+				}
+				let playlistIdModified = 'UU' + playlistId.slice(2, playlistId.length)
+				console.log('Getting latest videos for playlistIdModified: ' + playlistIdModified)
+				let resp = await axios.get('https://youtube.googleapis.com/youtube/v3/playlistItems',{
+					params: {
+						playlistId: playlistIdModified,
+						part: 'snippet,contentDetails',
 						key: process.env.API_KEY
 					}
+				}).catch(err => {
+					console.error(err.response.data.error)
 				})
-				if (searchResp && searchResp.data?.items?.length) {
-					playlistId = searchResp.data.items[0].snippet.channelId
-				}
-			}
-			let playlistIdModified = 'UU' + playlistId.slice(2, playlistId.length)
-			console.log('Getting latest videos for playlistIdModified: ' + playlistIdModified)
-			let resp = await axios.get('https://youtube.googleapis.com/youtube/v3/playlistItems',{
-				params: {
-					playlistId: playlistIdModified,
-					part: 'snippet,contentDetails',
-					key: process.env.API_KEY
-				}
-			}).catch(err => {
-				console.error(err.response.data.error)
-			})
 
-			if (resp && resp.status == 200 && resp.data?.items?.length) {
-				let data = resp.data
-				let items = data.items
-				
-				console.log('Found', items.length, 'videos')
-				for (let item of items) {
-					videos.push(item)
-				}
-			} 
-			
+				if (resp && resp.status == 200 && resp.data?.items?.length) {
+					let data = resp.data
+					let items = data.items
+
+					console.log('Found', items.length, 'videos')
+					for (let item of items) {
+						videos.push(item)
+					}
+				} 
+			}
 		}
 
 		// sort videos by date
@@ -82,7 +84,7 @@ app.get('/v1/videos', async (req, res) => {
 			return new Date(b.contentDetails.videoPublishedAt) - new Date(a.contentDetails.videoPublishedAt)
 		})
 
-		console.log('Found', videos.length, 'videos')
+		console.log('Found', videos.length, 'videos total')
 
 		res.status(200).json({
 			count: videos.length,
